9c25f20ffc3b3a25a87a27505b042adeeeddbbbf
Boris Scheiman <bscheiman@gmail.com>
2014-09-23 15:07:10 -0500

    Map page

---
+++ SingleLocationMap/SingleLocationMapPage.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms.Maps;
+using Xamarin.Forms;
+
+namespace App.Pages {
+	public class SingleLocationMapPage : BasePage {
+		public SingleLocationMapPage() {
+			Title = "Map";
+
+			Content = ScrollWrap(new Map(MapSpan.FromCenterAndRadius(new Position($LAT$, $LNG$), Distance.FromMeters(100))) {
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand
+			});
+		}
+	}
+}
+
